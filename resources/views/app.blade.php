cb81abbacb32d5684c2ec78d419b589abf68dcc7
Nat-r <nrosevegue@edenschool.fr>
2021-10-05 15:57:19 +0200

    Save Structure Scss/Js

---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>OZA</title>
+
+    <link rel="stylesheet" href="/css/lib/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/main.min.css">
+</head>
+<body>
+<div class="content">
+    @yield('content')
+</div>
+
+<script src="/js/lib/jquery-3.3.1.min.js"></script>
+<script src="/js/lib/bootstrap.min.js"></script>
+@yield('script')
+</body>
+</html>
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+
+@section('content')
+
+@endsection
+
+@section('script')
+@endsection
