b4d6ca5b3a14f3f63062390c5a180e489834c407
AnthonyVallenet <avallenet@edenschool.fr>
2021-12-15 13:41:51 +0100

    change route

--- routes/web.php
+++ routes/web.php
@@ -49,14 +49,9 @@ Route::middleware(['auth'])->group(function() {
             OZA Section
     ===============================*/
 
-    /*================ ADMIN ================*/
-    Route::middleware(['permission:oza,ADMIN'])->group(function () {
+    /*================ ADMIN | EXPERT ================*/
+    Route::middleware(['permission:oza,ADMIN,EXPERT'])->group(function () {
         Route::get('/users', [UserAdminController::class, 'index'])->name('admin.user');
-        Route::get('/users/create', [UserAdminController::class, 'create'])->name('admin.user.create');
-        Route::get('/users/{user}/edit', [UserAdminController::class, 'edit'])->name('admin.user.edit');
-
-        Route::post('/users/store', [UserAdminController::class, 'store'])->name('admin.user.store');
-        Route::post('/users/{user}/update', [UserAdminController::class, 'update'])->name('admin.user.update');
 
         Route::get('/client', [ClientAdminController::class, 'index'])->name('admin.client');
         Route::get('/client/create', [ClientAdminController::class, 'create'])->name('admin.client.create');
@@ -65,17 +60,31 @@ Route::middleware(['auth'])->group(function() {
         Route::post('/client/store', [ClientAdminController::class, 'store'])->name('admin.client.store');
         Route::post('/client/{client}/update', [ClientAdminController::class, 'update'])->name('admin.client.update');
 
-        Route::post('/client/{client}/single_document/store', [SingleDocumentAdminController::class, 'store'])->name('admin.single_document.store');
-
         Route::get('/clients/du', [SingleDocumentAdminController::class, 'index'])->name('admin.client.single_document');
     });
 
 
-    Route::get('/{doc_name}', [DocController::class, 'index'])->name('documentation');
-    Route::get('/{doc_name}/edit', [DocController::class, 'edit'])->name('documentation.edit');
+    /*================ ADMIN ================*/
+    Route::middleware(['permission:oza,ADMIN'])->group(function () {
+        Route::get('/users/create', [UserAdminController::class, 'create'])->name('admin.user.create');
+        Route::get('/users/{user}/edit', [UserAdminController::class, 'edit'])->name('admin.user.edit');
 
-    Route::post('/{doc_name}/update', [DocController::class, 'update'])->name('documentation.update');
-    Route::post('/doc/upload', [DocController::class, 'upload'])->name('documentation.upload');
+        Route::post('/users/store', [UserAdminController::class, 'store'])->name('admin.user.store');
+        Route::post('/users/{user}/update', [UserAdminController::class, 'update'])->name('admin.user.update');
+
+
+        Route::post('/client/{client}/single_document/store', [SingleDocumentAdminController::class, 'store'])->name('admin.single_document.store');
+    });
+
+
+    Route::get('/{doc_name}', [DocController::class, 'index'])->name('documentation');
+
+    Route::middleware(['permission:oza,ADMIN'])->group(function () {
+        Route::get('/{doc_name}/edit', [DocController::class, 'edit'])->name('documentation.edit');
+
+        Route::post('/{doc_name}/update', [DocController::class, 'update'])->name('documentation.update');
+        Route::post('/doc/upload', [DocController::class, 'upload'])->name('documentation.upload');
+    });
 
 
     /*===============================
