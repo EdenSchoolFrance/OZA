3b3d79b476a38fd53f26cd140a43b4b0e9c445e3
Nat-r <nrosevegue@edenschool.fr>
2022-02-24 16:41:33 +0100

    PDF Add page

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -24,7 +24,7 @@ class PDFController extends Controller
 
         $config = '{type:"pie",data:{labels:["Acceptable","A améliorer","Agir vite","STOP"],datasets:[{data:['.$single_document->graphique()[0].','.$single_document->graphique()[1].','.$single_document->graphique()[2].','.$single_document->graphique()[3].']}]},options:{layout:{padding:0,},plugins:{legend:{display:true,position:"right",labels:{boxHeight:45,boxWidth:45,},title:{display:false,}}}}}';
         $chartUrl = 'https://quickchart.io/chart?&w=500&h=300&c='.urlencode($config);
-        return PDF::loadView('app.pdf.index', compact('chartUrl','single_document','item_mat','item_veh','item_eng','sd_risks'))->setPaper('a4', 'landscape')->download('DU-OZA.pdf');
+        return PDF::loadView('app.pdf.index', compact('chartUrl','single_document','item_mat','item_veh','item_eng','sd_risks'))->setPaper('a4', 'landscape')->stream();
     }
 
     public static function create(){
