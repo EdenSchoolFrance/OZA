3f0cc6fc4e2989130d69bc06a4cfaa1efbe96a9f
Nat-r <nrosevegue@edenschool.fr>
2022-07-08 15:55:50 +0200

    Import 60%

--- app/Imports/FirstSheetImport.php
+++ app/Imports/FirstSheetImport.php
@@ -3,7 +3,9 @@
 namespace App\Imports;
 
 use App\Models\Danger;
+use App\Models\SdDanger;
 use App\Models\SdRisk;
+use App\Models\SdWorkUnit;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 
@@ -36,9 +38,12 @@ class FirstSheetImport implements ToCollection
              * $impact = $collection[$i][9];
              */
 
-            $this->lockDanger($collection[$i][3]);
+            $sd_danger = $this->lockDanger($collection[$i][4]);
+            $sd_work_unit = $this->lockWorkUnit($collection[$i][3]);
 
             $data = [
+                "danger" => $sd_danger,
+                "work_unit" => $sd_work_unit,
                 "risk" => $collection[$i][5],
                 "frequency" => $collection[$i][6],
                 "probability" => $collection[$i][7],
@@ -49,7 +54,6 @@ class FirstSheetImport implements ToCollection
             $risk = $this->createRisk($data);
 
             $this->deleteTab[] = $risk->id;
-
 
         }
     }
@@ -86,8 +90,47 @@ class FirstSheetImport implements ToCollection
             "F" => "female",
             "H" => "null"
         ];
+        $frequency = "year+";
+        $probability = "very weak";
+        $gravity = "death";
+        $impact = "null";
 
+        for ($i = 0; $i < count($fre); $i++){
+            if ($fre[$i] === $data["frequency"]){
+                $frequency = $fre[$i];
+            }
+        }
 
+        for ($i = 0; $i < count($pro); $i++){
+            if ($pro[$i] === $data["probability"]){
+                $probability = $pro[$i];
+            }
+        }
+
+        for ($i = 0; $i < count($gra); $i++){
+            if ($gra[$i] === $data["gravity"]){
+                $gravity = $gra[$i];
+            }
+        }
+
+        for ($i = 0; $i < count($imp); $i++){
+            if ($imp[$i] === $data["impact"]){
+                $impact = $imp[$i];
+            }
+        }
+
+        $sd_risk = new SdRisk();
+        $sd_risk->id = uniqid();
+        $sd_risk->name = $data["risk"];
+        $sd_risk->frequency = $frequency;
+        $sd_risk->probability = $probability;
+        $sd_risk->gravity = $gravity;
+        $sd_risk->impact = $impact;
+        $sd_risk->sd_danger()->associate($data["danger"]);
+        if ($data["work_unit"] !== null) $sd_risk->sd_work_unit()->associate($data["work_unit"]);
+        $sd_risk->save();
+
+        return $sd_risk;
 
 
 
@@ -96,16 +139,28 @@ class FirstSheetImport implements ToCollection
     protected function lockWorkUnit($data)
     {
 
+        if ($data === "Tous"){
+            return null;
+        }else{
+            $single_document = $this->single_document;
+            $sd_work_unit = SdWorkUnit::where('name', $data)->whereHas('single_document', function ($q) use ($single_document){
+                $q->where('id', $single_document->id);
+            })->first();
+            if (!$sd_work_unit) $this->deleteAll("WorkUnit introuvable");
+            return $sd_work_unit;
+        }
+
     }
 
     protected function lockDanger($data)
     {
         $single_document = $this->single_document;
-        $danger = Danger::where('info',$data)->whereHas('single_document', function ($q) use ($single_document){
+        $danger = Danger::where('info', $data)->first();
+        $sd_danger = SdDanger::where('danger_id', $danger->id)->whereHas('single_document', function ($q) use ($single_document){
             $q->where('id', $single_document->id);
         })->first();
-        if (!$danger) $this->deleteAll("");
-        return $danger;
+        if (!$danger || !$sd_danger) $this->deleteAll("Danger introuvable !");
+        return $sd_danger;
     }
 
     protected function deleteAll($dataError)
